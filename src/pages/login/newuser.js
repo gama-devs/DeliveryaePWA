ad560d3c3f0d91319a2f5cde6cbe6450748a1a25
Wictorgirardi <wictor.girardi@gmail.com>
2020-06-16 22:54:05 -0300

    absolutamente cansei de css por hoje

--- src/pages/login/newuser.js
+++ src/pages/login/newuser.js
@@ -1,88 +1,94 @@
-import React, { useEffect, useState } from 'react'
-import { withRouter } from 'react-router-dom'
-import Loader from 'react-loader-spinner'
-import { Input } from 'reactstrap'
-import VisibilityOffIcon from '@material-ui/icons/VisibilityOff'
-import VisibilityIcon from '@material-ui/icons/Visibility'
-import NumberFormat from 'react-number-format'
-
+import React, { useEffect, useState } from "react";
+import { withRouter } from "react-router-dom";
+import Loader from "react-loader-spinner";
+import { Input, Row } from "reactstrap";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import NumberFormat from "react-number-format";
+import { BrowserView, MobileView } from "react-device-detect";
+import logo from "../../assets/logo.png";
 const NewUser = (props) => {
-	const [loadingreq, setloading] = useState(false)
-	const [showpassword, setshowpass] = useState(false)
+  const [loadingreq, setloading] = useState(false);
+  const [showpassword, setshowpass] = useState(false);
 
-	useEffect(() => {
-		// console.log('usereffect try')
-		// console.log(props.backarrowstate, 'estadoo')
-		if (props.backarrowstate === 'newaccount') {
-			// console.log('em tese foi')
-			props.setbackarrow('main')
-		}
-	}, [props.backarrowstate])
-	return (
-		<div
-			style={{
-				textAlign: 'center',
-				flex: 1,
-				position: 'fixed',
-				bottom: '0',
-				height: '70vh',
-				width: '100vw',
-				borderRadius: '32px',
-				backgroundColor: '#FFF',
-			}}
-		>
-			<div
-				style={{
-					flex: 1,
-					marginTop: '3vh',
-					fontSize: '14px',
-					fontWeight: 'bold',
-					color: '#FF805D',
-				}}
-			>
-				Vamos criar sua conta?
-				<br /> É rápido! Igual nossa entrega.
-			</div>
-			<div
-				style={{
-					flex: 1,
-					marginTop: '3vh',
-					fontSize: '12px',
-					fontWeight: 'bold',
-					color: '#000',
-				}}
-			>
-				Preencha os dados abaixo e <br />
-				enviariamos uma confirmação via SMS.
-			</div>
-			<NumberFormat
-				style={{
-					backgroundColor: '#EDF1F7',
-					borderRadius: '10px',
-					margin: '0 auto',
-					width: '80vw',
-					height: '7vh',
-					marginTop: '3vh',
-					border: 'none',
-					fontSize: '16px'
-				}}
-				className="form-control"
-				placeholder="Seu número de celular"
-				format="(##) # ####-####"
-				mask="_"
-				onChange={async (e) => {
-					let real = e.target.value
-					real = real.split('.').join('')
-					real = real.split('/').join('')
-					real = real.split('-').join('')
-					real = real.split('(').join('')
-					real = real.split(')').join('')
-					real = real.split(' ').join('')
-					real = real.split('X').join('')
-					props.handlestate('celnumber', real)
-				}}
-			/>
-			{/* <Input
+  useEffect(
+    () => {
+      // console.log('usereffect try')
+      // console.log(props.backarrowstate, 'estadoo')
+      if (props.backarrowstate === "newaccount") {
+        // console.log('em tese foi')
+        props.setbackarrow("main");
+      }
+    },
+    [props.backarrowstate]
+  );
+  return (
+    <div>
+      <MobileView>
+        <div
+          style={{
+            textAlign: "center",
+            flex: 1,
+            position: "fixed",
+            bottom: "0",
+            height: "70vh",
+            width: "100vw",
+            borderRadius: "32px",
+            backgroundColor: "#FFF",
+          }}
+        >
+          <div
+            style={{
+              flex: 1,
+              marginTop: "3vh",
+              fontSize: "14px",
+              fontWeight: "bold",
+              color: "#FF805D",
+            }}
+          >
+            Vamos criar sua conta?
+            <br /> É rápido! Igual nossa entrega.
+          </div>
+          <div
+            style={{
+              flex: 1,
+              marginTop: "3vh",
+              fontSize: "12px",
+              fontWeight: "bold",
+              color: "#000",
+            }}
+          >
+            Preencha os dados abaixo e <br />
+            enviariamos uma confirmação via SMS.
+          </div>
+          <NumberFormat
+            style={{
+              backgroundColor: "#EDF1F7",
+              borderRadius: "10px",
+              margin: "0 auto",
+              width: "80vw",
+              height: "7vh",
+              marginTop: "3vh",
+              border: "none",
+              fontSize: "16px",
+            }}
+            className="form-control"
+            placeholder="Seu número de celular"
+            format="(##) # ####-####"
+            mask="_"
+            onChange={async (e) => {
+              let real = e.target.value;
+              real = real.split(".").join("");
+              real = real.split("/").join("");
+              real = real.split("-").join("");
+              real = real.split("(").join("");
+              real = real.split(")").join("");
+              real = real.split(" ").join("");
+              real = real.split("X").join("");
+              props.handlestate("celnumber", real);
+            }}
+          />
+          {/* <Input
 				style={{
 					backgroundColor: '#EDF1F7',
 					borderRadius: '10px',
@@ -97,116 +103,328 @@ const NewUser = (props) => {
 					props.handlestate('celnumber', e.target.value)
 				}}
 			/> */}
-			<Input
+          <Input
+            style={{
+              backgroundColor: "#EDF1F7",
+              borderRadius: "10px",
+              margin: "0 auto",
+              width: "80vw",
+              height: "7vh",
+              marginTop: "1vh",
+              border: "none",
+              fontSize: "16px",
+            }}
+            placeholder="Seu e-mail"
+            onChange={(e) => {
+              props.handlestate("email", e.target.value);
+            }}
+          />
+          <Input
+            style={{
+              backgroundColor: "#EDF1F7",
+              borderRadius: "10px",
+              margin: "0 auto",
+              width: "80vw",
+              height: "7vh",
+              marginTop: "1vh",
+              border: "none",
+              fontSize: "16px",
+            }}
+            placeholder="Seu nome"
+            onChange={(e) => {
+              props.handlestate("nome", e.target.value);
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: "#EDF1F7",
+              borderRadius: "12px",
+              margin: "1vh auto 0",
+              width: "80vw",
+              height: "7vh",
+            }}
+          >
+            <Input
+              type={showpassword ? "text" : "password"}
+              style={{
+                backgroundColor: "#EDF1F7",
+                border: "none",
+                fontSize: "16px",
+              }}
+              placeholder="Escolha uma senha"
+              onChange={(e) => {
+                props.handlestate("password", e.target.value);
+              }}
+            />
+            <div
+              style={{ marginRight: "2vw" }}
+              onClick={() => {
+                setshowpass(() => {
+                  console.log(showpassword);
+                  return !showpassword;
+                });
+              }}
+            >
+              {showpassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+            </div>
+          </div>
+          <div
+            onClick={async () => {
+              try {
+                setloading(true);
+                // console.log(props.loginstate, 'estadoooooo')
+                await props.handlenewuser();
+                props.handlepagestate("confirmsms");
+                props.setbackarrow("newaccount");
+                setloading(false);
+              } catch {
+                setloading(false);
+              }
+            }}
+            style={{
+              cursor: "pointer",
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center",
+              width: "100vw",
+              borderRadius: "32px 32px 0px 0px",
+              height: "12vh",
+              display: "flex",
+              backgroundColor: "#FF805D",
+              position: "absolute",
+              left: "0",
+              bottom: "0",
+            }}
+          >
+            {loadingreq ? (
+              <Loader type="Oval" color={"#FFF"} height="5vh" width="5vh" />
+            ) : (
+              <h4
+                style={{
+                  color: "#FFF",
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                }}
+              >
+                Cadastrar
+              </h4>
+            )}
+          </div>
+        </div>
+      </MobileView>
+      <BrowserView>
+        <div
+          style={{
+            textAlign: "center",
+            flex: 1,
+            bottom: "0",
+            height: "70vh",
+            borderRadius: "32px",
+            backgroundColor: "#FFF",
+          }}
+        >
+          <img
+            src={logo}
+            style={{
+              width: "300px",
+              marginTop: "-350px",
+            }}
+          />
+          <div
+            style={{
+              flex: 1,
+              marginTop: "-80px",
+              fontSize: "20px",
+              fontWeight: "bold",
+              color: "#FF805D",
+            }}
+          >
+            Vamos criar sua conta?
+            <br /> É rápido! Igual nossa entrega.
+          </div>
+          <div
+            style={{
+              flex: 1,
+              marginTop: "3vh",
+              fontSize: "16px",
+              fontWeight: "bold",
+              color: "#000",
+            }}
+          >
+            Preencha os dados abaixo e <br />
+            enviariamos uma confirmação via SMS.
+          </div>
+          <NumberFormat
+            style={{
+              backgroundColor: "#EDF1F7",
+              borderRadius: "10px",
+              margin: "0 auto",
+              width: "100%",
+
+              height: "7vh",
+              marginTop: "3vh",
+              border: "none",
+              fontSize: "16px",
+            }}
+            className="form-control"
+            placeholder="Seu número de celular"
+            format="(##) # ####-####"
+            mask="_"
+            onChange={async (e) => {
+              let real = e.target.value;
+              real = real.split(".").join("");
+              real = real.split("/").join("");
+              real = real.split("-").join("");
+              real = real.split("(").join("");
+              real = real.split(")").join("");
+              real = real.split(" ").join("");
+              real = real.split("X").join("");
+              props.handlestate("celnumber", real);
+            }}
+          />
+          {/* <Input
 				style={{
 					backgroundColor: '#EDF1F7',
 					borderRadius: '10px',
 					margin: '0 auto',
 					width: '80vw',
 					height: '7vh',
-					marginTop: '1vh',
+					marginTop: '3vh',
 					border: 'none',
-					fontSize: '16px'
 				}}
-				placeholder="Seu e-mail"
+				placeholder="Seu número de celular"
 				onChange={(e) => {
-					props.handlestate('email', e.target.value)
+					props.handlestate('celnumber', e.target.value)
 				}}
-			/>
-			<Input
-				style={{
-					backgroundColor: '#EDF1F7',
-					borderRadius: '10px',
-					margin: '0 auto',
-					width: '80vw',
-					height: '7vh',
-					marginTop: '1vh',
-					border: 'none',
-					fontSize: '16px'
-				}}
-				placeholder="Seu nome"
-				onChange={(e) => {
-					props.handlestate('nome', e.target.value)
-				}}
-			/>
-			<div
-				style={{
-					display: 'flex',
-					alignItems: 'center',
-					backgroundColor: '#EDF1F7',
-					borderRadius: '12px',
-					margin: '1vh auto 0',
-					width: '80vw',
-					height: '7vh',
-				}}
-			>
-				<Input
-					type={showpassword ? 'text' : 'password'}
-					style={{
-						backgroundColor: '#EDF1F7',
-						border: 'none',
-						fontSize: '16px'
-					}}
-					placeholder="Escolha uma senha"
-					onChange={(e) => {
-						props.handlestate('password', e.target.value)
-					}}
-				/>
-				<div
-					style={{ marginRight: '2vw' }}
-					onClick={() => {
-						setshowpass(() => {
-							console.log(showpassword)
-							return !showpassword
-						})
-					}}
-				>
-					{showpassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-				</div>
-			</div>
-			<div
-				onClick={async () => {
-					try {
-						setloading(true)
-						// console.log(props.loginstate, 'estadoooooo')
-						await props.handlenewuser()
-						props.handlepagestate('confirmsms')
-						props.setbackarrow('newaccount')
-						setloading(false)
-					} catch {
-						setloading(false)
-					}
-				}}
-				style={{
-					cursor: 'pointer',
-					alignItems: 'center',
-					justifyContent: 'center',
-					textAlign: 'center',
-					width: '100vw',
-					borderRadius: '32px 32px 0px 0px',
-					height: '12vh',
-					display: 'flex',
-					backgroundColor: '#FF805D',
-					position: 'absolute',
-					left: '0',
-					bottom: '0',
-				}}
-			>
-				{loadingreq ? (
-					<Loader type="Oval" color={'#FFF'} height="5vh" width="5vh" />
-				) : (
-					<h4
-						style={{
-							color: '#FFF',
-							fontWeight: 'bold',
-							fontSize: '15px',
-						}}
-					>
-						Cadastrar
-					</h4>
-				)}
-			</div>
-		</div>
-	)
-}
-export default withRouter(NewUser)
+			/> */}
+          <Input
+            style={{
+              backgroundColor: "#EDF1F7",
+              borderRadius: "10px",
+              margin: "0 auto",
+              width: "100%",
+              height: "7vh",
+              marginTop: "1vh",
+
+              border: "none",
+              fontSize: "16px",
+            }}
+            placeholder="Seu e-mail"
+            onChange={(e) => {
+              props.handlestate("email", e.target.value);
+            }}
+          />
+          <Input
+            style={{
+              backgroundColor: "#EDF1F7",
+              borderRadius: "10px",
+              margin: "0 auto",
+              width: "100%",
+              height: "7vh",
+              marginTop: "1vh",
+
+              border: "none",
+              fontSize: "16px",
+            }}
+            placeholder="Seu nome"
+            onChange={(e) => {
+              props.handlestate("nome", e.target.value);
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: "#EDF1F7",
+              borderRadius: "12px",
+              margin: "1vh auto 0",
+
+              width: "100%",
+              height: "7vh",
+            }}
+          >
+            <Input
+              type={showpassword ? "text" : "password"}
+              style={{
+                backgroundColor: "#EDF1F7",
+                border: "none",
+                fontSize: "16px",
+              }}
+              placeholder="Escolha uma senha"
+              onChange={(e) => {
+                props.handlestate("password", e.target.value);
+              }}
+            />
+            <div
+              style={{ marginRight: "2vw" }}
+              onClick={() => {
+                setshowpass(() => {
+                  console.log(showpassword);
+                  return !showpassword;
+                });
+              }}
+            >
+              {showpassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+            </div>
+          </div>
+          <div
+            onClick={async () => {
+              try {
+                setloading(true);
+                // console.log(props.loginstate, 'estadoooooo')
+                await props.handlenewuser();
+                props.handlepagestate("confirmsms");
+                props.setbackarrow("newaccount");
+                setloading(false);
+              } catch {
+                setloading(false);
+              }
+            }}
+            style={{
+              cursor: "poniter",
+              alignItems: "center",
+              justifyContent: "center",
+              flex: 1,
+              width: "100%",
+              borderRadius: "12px 12px 12px 12px",
+              height: "55px",
+			  display: "flex",
+			  marginBottom: '20px',
+              backgroundColor: "#FF805D",
+              position: "relative",
+              marginTop: "40px",
+            }}
+          >
+            {loadingreq ? (
+              <Loader type="Oval" color={"#FFF"} height="5vh" width="5vh" />
+            ) : (
+              <h4
+                style={{
+                  color: "#FFF",
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                }}
+              >
+                Cadastrar
+              </h4>
+            )}
+          </div>
+		  <Row>
+            <span
+              style={{
+                textDecoration: "underline",
+                textAlign: "center",
+                margin: "auto",
+              }}
+            >
+              Voltar
+            </span>
+          </Row>
+        </div>
+      </BrowserView>
+    </div>
+  );
+};
+export default withRouter(NewUser);
